cbf3006d6c72957c794c17932cac8847c4f8fde1
Bo-zhou4900 <zhoubo1233@hotmail.com>
2024-11-11 00:50:56 -0800

    add the bcrypt_elixir dependency

--- lib/term_project/accounts/user.ex
+++ lib/term_project/accounts/user.ex
@@ -5,15 +5,25 @@ defmodule TermProject.Accounts.User do
   schema "users" do
     field :name, :string
     field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
 
-    timestamps(type: :utc_datetime)
+    timestamps()
   end
 
-  @doc false
-  def changeset(user, attrs) do
+  def registration_changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email, :password_hash])
-    |> validate_required([:name, :email, :password_hash])
+    |> cast(attrs, [:name, :email, :password])
+    |> validate_required([:name, :email, :password])
+    |> validate_length(:password, min: 6)
+    |> unique_constraint(:email)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case get_change(changeset, :password) do
+      nil -> changeset
+      password -> put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(password))
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule TermProject.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:uuid, "~> 1.1"} # UUID library for generating unique IDs
+      {:uuid, "~> 1.1"}, # UUID library for generating unique IDs
+      {:bcrypt_elixir, "~> 3.0"}
     ]
   end
 
