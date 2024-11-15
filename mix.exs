d042bbaf66300d65545899f62ebfafd09c75e775
Ruben Gill <rgill288@my.bcit.ca>
2024-11-14 22:09:18 -0800

    Added git ignore

--- mix.exs
+++ mix.exs
@@ -59,7 +59,9 @@ defmodule TermProject.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:uuid, "~> 1.1"} # UUID library for generating unique IDs
+      {:uuid, "~> 1.1"}, # UUID library for generating unique IDs
+      {:bcrypt_elixir, "~> 3.0"},
+      {:phoenix_html_helpers, "~> 1.0"}
     ]
   end
 
