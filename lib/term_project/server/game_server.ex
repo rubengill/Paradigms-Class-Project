4fb01dd1509a9b677d1237221d91cb4727929f5d
Ruben Gill <rgill288@my.bcit.ca>
2024-12-06 01:40:35 -0800

    Added code to recieve requests to join either a public or private lobby

--- lib/term_project/server/game_server.ex
+++ lib/term_project/server/game_server.ex
@@ -5,6 +5,21 @@ defmodule TermProject.Server.GameServer do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
+  @impl true
+  def init(:ok) do
+    {:ok, {}}
+  end
+
+  # If `password` is an empty string or nil, this is a public lobby request.
+  def request_lobby(username, password) when is_binary(password) and password != "" do
+    GenServer.call(__MODULE__, {:request_private_lobby, username, password})
+  end
+
+  #If `password` is a non-empty string or nil, this is a private lobby request.
+  def request_lobby(username, _password) do
+    GenServer.call(__MODULE__, {:request_public_lobby, username})
+  end
+
   def search_lobby() do
   end
 
@@ -13,10 +28,5 @@ defmodule TermProject.Server.GameServer do
   end
 
   def remove_lobby() do
-  end
-
-  @impl true
-  def init(:ok) do
-    {:ok, {}}
   end
 end
