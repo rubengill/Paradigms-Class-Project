02a8a82b142129828420f77a1b599f6ad31d18f0
Tarasios <cwilliams153@my.bcit.ca>
2024-12-09 00:10:30 -0800

    Update cavalry.ex

--- lib/term_project/game/unit/unit_types/cavalry.ex
+++ lib/term_project/game/unit/unit_types/cavalry.ex
@@ -1,42 +1,41 @@
-defmodule TermProject.Game.UnitTypes.Cavalry do
-  @moduledoc """
-  Cavalry unit implementation.
-  """
+defmodule TermProject.Units.Cavalry do
+  @behaviour TermProject.Unit
 
-  @behaviour TermProject.Game.Unit
+  @type t :: %__MODULE__{
+          type: atom(),
+          health: integer(),
+          damage: integer(),
+          range: integer(),
+          speed: integer()
+        }
 
-  alias TermProject.Utils.Position
+  defstruct type: :cavalry, health: 75, damage: 15, range: 2, speed: 3
 
   @impl true
-  def init(opts) do
+  def type, do: :cavalry
+
+  @impl true
+  def stats do
     %{
-      id: opts[:id],
-      type: :cavalry,
-      position: opts[:position],
-      owner: opts[:owner],
-      health: 150,
-      damage: 20,
-      speed: 2.0,
-      range: 1.5
+      health: 75,
+      damage: 15,
+      range: 2,
+      speed: 3
     }
   end
 
   @impl true
-  def move(unit) do
-    target_position = Position.enemy_base(unit.owner)
-    new_position = Position.move_towards(unit.position, target_position, unit.speed)
-    %{unit | position: new_position}
+  def attack(target) do
+    # Reduce target's health by the Cavalry's damage
+    %{target | health: target.health - 15}
   end
 
   @impl true
-  def attack(unit, target) do
-    # Cavalry may have special attack logic
-    new_target = %{target | health: target.health - unit.damage}
-    {unit, new_target}
+  def in_range?({x1, y1}, {x2, y2}) do
+    distance({x1, y1}, {x2, y2}) <= 2
   end
 
-  @impl true
-  def in_range?(unit, target) do
-    Position.distance(unit.position, target.position) <= unit.range
+  defp distance({x1, y1}, {x2, y2}) do
+    :math.sqrt(:math.pow(x2 - x1, 2) + :math.pow(y2 - y1, 2))
   end
 end
