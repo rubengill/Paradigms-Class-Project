ac861603630b9e8895240fddc77ec1d3bcf9c0e9
Marvin Sio <marvinbcit@gmail.com>
2024-12-11 02:29:26 -0800

    refactor game to use match id and state
    add mock server for testing

--- lib/term_project/application.ex
+++ lib/term_project/application.ex
@@ -14,7 +14,7 @@ defmodule TermProject.Application do
       # Start the Endpoint (http/https)
       TermProjectWeb.Endpoint,
       # Start the Game server
-      TermProject.Game
+      {TermProject.Game, :test_match_id}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html for strategies and options
