9404e14c21cbfc137932d74c0e8db91336f3fd61
Tarasios <sircassiel@gmail.com>
2024-12-11 21:11:17 -0800

    Update unit_costs.ex

--- lib/term_project/game/unit_costs.ex
+++ lib/term_project/game/unit_costs.ex
@@ -1,5 +1,5 @@
 defmodule TermProject.UnitCosts do
-  def cost(:soldier), do: %{wood: 50, iron: 30}
+  def cost(:knight), do: %{wood: 50, iron: 30}
   def cost(:archer), do: %{wood: 30, stone: 20}
   def cost(:cavalry), do: %{wood: 40, iron: 40}
 end
